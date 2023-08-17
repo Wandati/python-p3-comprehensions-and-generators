3fbcbe3171667d5f5878292fff15b86715d696e6
Wandati <Petersimon801@gmail.com>
2023-08-17 12:04:31 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # new_no = []
+    # for n in num_list:
+    #     if n % 2 == 0:
+    #         new_no.append(n)
+    # print(new_no)
+    return[n for n in num_list if n % 2 == 0 ]
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [n + "!" for n in sentence_list]
+
+
